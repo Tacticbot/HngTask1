449a8fe18ceb2959837b2a1b680467c6e7fc4d0c
Musa Okai <imusa4918@gmail.com>
2023-09-10 14:04:06 +0100

    Hng1

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from datetime import datetime
+import pytz
+
+app = FastAPI()
+
+@app.get("/api")
+async def api(slack_name: str, track: str):
+    response_data = {
+        "slack_name": slack_name,
+        "current_day": datetime.now().strftime('%A'),
+        "utc_time": datetime.now(pytz.timezone('UTC') ).strftime('%Y-%m-%dT%H:%M:%SZ'),
+        "track": track,
+        "github_file_url": "https://github.com/Tacticbot/HngTask1/blob/main/main.py",
+        "github_repo_url": "https://github.com/Tacticbot/HngTask1",
+        "status_code": 200
+        }
+    return response_data
